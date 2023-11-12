55e8146d4d9337c11abe7b748fc9c0ddb0c9ffa5
dy102 <ekagnlek2@gmail.com>
2023-11-12 17:48:33 +0900

    feat: 증정 이벤트

---
+++ src/main/java/christmas/domain/event/GiveAwayEvent.java
@@ -0,0 +1,20 @@
+package christmas.domain.event;
+
+import christmas.domain.EventParameter;
+
+public class GiveAwayEvent implements Event, GiveEvent {
+
+    public int apply(EventParameter eventParameter) {
+        if (isApply(eventParameter.totalPrice().price())) {
+            return (-1) * 25000;
+        }
+        return 0;
+    }
+
+    public boolean isApply(int price) {
+        if (price > 120000) {
+            return true;
+        }
+        return false;
+    }
+}
