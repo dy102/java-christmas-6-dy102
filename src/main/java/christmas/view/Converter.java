d2d33a5b8e7f64149c33a2bd34f1c71a4b17e531
dy102 <ekagnlek2@gmail.com>
2023-11-12 19:07:40 +0900

    refactor: 변환의 책임을 Converter로 넘김

--- src/main/java/christmas/view/Converter.java
+++ src/main/java/christmas/view/Converter.java
@@ -1,11 +1,15 @@
 package christmas.view;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static christmas.option.Error.ILLEGAL_DATE;
+import static christmas.option.Error.ILLEGAL_ORDER;
 
 public class Converter {
+    private static final String DELIMITER_COMMA = ",";
+    private static final String DELIMITER_DASH = "-";
     private Converter() {
     }
 
@@ -18,6 +22,27 @@ public class Converter {
     }
 
     public static List<String> stringToList(String order) {
-        return Arrays.stream(order.split(",")).toList();
+        return Arrays.stream(order.split(DELIMITER_COMMA)).toList();
+    }
+
+    public static List<String> orderToMenuNames(List<String> orders){
+        List<String> menuNames = new ArrayList<>();
+        for (String order : orders) {
+            Arrays.stream(order.split(DELIMITER_DASH)).findFirst().ifPresent(menuNames::add);
+        }
+        return menuNames;
+    }
+
+    public static List<Integer> orderToMenuCount(List<String> orders){
+        List<Integer> menuCount = new ArrayList<>();
+        for (String order : orders) {
+            List<String> splitOrder = Arrays.stream(order.split(DELIMITER_DASH)).toList();
+            try {
+                menuCount.add(Integer.parseInt(splitOrder.get(1)));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(ILLEGAL_ORDER.getMessage());
+            }
+        }
+        return menuCount;
     }
 }
