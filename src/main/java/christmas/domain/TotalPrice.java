366d83fb109435023ae34296a9fecdf2a7437d08
dy102 <ekagnlek2@gmail.com>
2023-11-12 17:43:54 +0900

    feat: 할인 전 후 총 금액을 계산하고 저장하는 기능

---
+++ src/main/java/christmas/domain/TotalPrice.java
@@ -0,0 +1,34 @@
+package christmas.domain;
+
+import christmas.option.Menu;
+
+import java.util.List;
+
+public class TotalPrice {
+    private int price = 0;
+
+    public void caculateTotalPrice(List<String> menuNames, List<Integer> menuCount) {
+        for (int i = 0; i < menuNames.size(); i++) {
+            Menu name = Menu.of(menuNames.get(i));
+            int count = menuCount.get(i);
+            price += name.getPrice() * count;
+        }
+    }
+
+    public int applyDiscount(int totalDiscount) {
+        price += totalDiscount;
+        return price;
+    }
+
+    public boolean isLessThanOne() {
+        return price < 10000;
+    }
+
+    public boolean isMoreThanTwelve() {
+        return price >= 120000;
+    }
+
+    public int price() {
+        return price;
+    }
+}
