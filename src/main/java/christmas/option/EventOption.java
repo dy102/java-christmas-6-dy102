9e51e855c66e88da063807747c2e8f0db1daac8d
dy102 <ekagnlek2@gmail.com>
2023-11-15 21:29:12 +0900

    refactor: 매직넘버 제거 및 상수 class 생성

---
+++ src/main/java/christmas/option/EventOption.java
@@ -0,0 +1,11 @@
+package christmas.option;
+
+public class EventOption {
+    public static final int START_DATE = 1;
+    public static final int END_DATE = 31;
+    public static final int MAXIMUM_BOUND_COUNT = 20;
+    public static final int EVENT_MONTH = 12;
+
+    private EventOption() {
+    }
+}
