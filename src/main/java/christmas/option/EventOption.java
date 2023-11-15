94a32e3c142a1766bec3ce39c5959c1702295573
dy102 <ekagnlek2@gmail.com>
2023-11-15 22:05:45 +0900

    refactor: 상수 추가(매직넘버 제거)

--- src/main/java/christmas/option/EventOption.java
+++ src/main/java/christmas/option/EventOption.java
@@ -5,6 +5,7 @@ public class EventOption {
     public static final int END_DATE = 31;
     public static final int MAXIMUM_BOUND_COUNT = 20;
     public static final int EVENT_MONTH = 12;
+    public static final int MINIMUM_PRICE_APPLY_EVENT = 10_000;
 
     private EventOption() {
     }
