33d3dfe3e17d230772b93e3ee77a1b8d510d86cb
dy102 <ekagnlek2@gmail.com>
2023-11-15 21:23:34 +0900

    refactor: 메서드명 변경

--- src/main/java/christmas/domain/benefit/EventPrice.java
+++ src/main/java/christmas/domain/benefit/EventPrice.java
@@ -1,6 +1,7 @@
-package christmas.domain;
+package christmas.domain.benefit;
 
 import christmas.domain.event.Event;
+import christmas.domain.user.UserInformation;
 import christmas.option.EventName;
 
 import java.util.ArrayList;
@@ -8,35 +9,35 @@ import java.util.Collections;
 import java.util.List;
 
 public class EventPrice {
-    private final EventParameter eventParameter;
+    private final UserInformation userInformation;
     private final List<String> appliedEvent = new ArrayList<>();
     private final List<Integer> appliedPrice = new ArrayList<>();
 
     private int discountPrice = 0;
     private int servicePrice = 0;
 
-    public EventPrice(EventParameter eventParameter) {
-        this.eventParameter = eventParameter;
+    public EventPrice(UserInformation userInformation) {
+        this.userInformation = userInformation;
     }
 
-    public void checkDiscount(Event event) {
-        if (eventParameter.totalPrice().price() < 10000) {//어디에 넘길지 고민좀..
+    public void calculateDiscountPrice(Event event) {
+        if (userInformation.totalPrice().price() < 10000) {//어디에 넘길지 고민좀..
             return;
         }
 
-        if (event.isApply(eventParameter.visitDate().date())) {
+        if (event.isApply(userInformation.visitDate().date())) {
             appliedEvent.add(EventName.of(event).getName());
-            int price = event.apply(eventParameter);
+            int price = event.apply(userInformation);
             appliedPrice.add(price);
 
             discountPrice += price;
         }
     }
 
-    public void checkGiveService(Event event) {
-        if (event.isApply(eventParameter.totalPrice().price())) {
+    public void calculateServicePrice(Event event) {
+        if (event.isApply(userInformation.totalPrice().price())) {
             appliedEvent.add(EventName.of(event).getName());
-            int price = event.apply(eventParameter);
+            int price = event.apply(userInformation);
             appliedPrice.add(price);
 
             servicePrice += price;
--- src/test/java/christmas/domain/benefit/EventPriceTest.java
+++ src/test/java/christmas/domain/benefit/EventPriceTest.java
@@ -1,7 +1,11 @@
-package christmas.domain;
+package christmas.domain.benefit;
 
 import christmas.domain.event.DDayEvent;
 import christmas.domain.event.GiveAwayEvent;
+import christmas.domain.user.TotalPrice;
+import christmas.domain.user.UserInformation;
+import christmas.domain.user.UserMenu;
+import christmas.domain.user.VisitDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,13 +20,13 @@ class EventPriceTest {
         VisitDate visitDate = new VisitDate(25);
         UserMenu userMenu = UserMenu.form(List.of("양송이수프-1", "제로콜라-1", "크리스마스파스타-1"));
         TotalPrice totalPrice = new TotalPrice();
-        totalPrice.caculateTotalPrice(userMenu.getMenuNames(), userMenu.getMenuCount());
+        totalPrice.caculateTotalPrice(userMenu.getMenuNames().names(), userMenu.getMenuCounts().counts());
 
-        EventParameter eventParameter = new EventParameter(visitDate, userMenu, totalPrice);
-        EventPrice eventPrice = new EventPrice(eventParameter);
+        UserInformation userInformation = new UserInformation(visitDate, userMenu, totalPrice);
+        EventPrice eventPrice = new EventPrice(userInformation);
         DDayEvent dDayEvent = new DDayEvent();
 
-        eventPrice.checkDiscount(dDayEvent);
+        eventPrice.calculateDiscountPrice(dDayEvent);
 
         assertThat(eventPrice.getDiscountPrice()).isEqualTo(-3400);
     }
@@ -33,13 +37,13 @@ class EventPriceTest {
         VisitDate visitDate = new VisitDate(25);
         UserMenu userMenu = UserMenu.form(List.of("크리스마스파스타-5"));
         TotalPrice totalPrice = new TotalPrice();
-        totalPrice.caculateTotalPrice(userMenu.getMenuNames(), userMenu.getMenuCount());
+        totalPrice.caculateTotalPrice(userMenu.getMenuNames().names(), userMenu.getMenuCounts().counts());
 
-        EventParameter eventParameter = new EventParameter(visitDate, userMenu, totalPrice);
-        EventPrice eventPrice = new EventPrice(eventParameter);
+        UserInformation userInformation = new UserInformation(visitDate, userMenu, totalPrice);
+        EventPrice eventPrice = new EventPrice(userInformation);
         GiveAwayEvent giveAwayEvent = new GiveAwayEvent();
 
-        eventPrice.checkGiveService(giveAwayEvent);
+        eventPrice.calculateServicePrice(giveAwayEvent);
 
         assertThat(eventPrice.getServicePrice()).isEqualTo(-25_000);
     }
